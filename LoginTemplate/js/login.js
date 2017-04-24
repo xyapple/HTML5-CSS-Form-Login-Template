1cc9391f28447c729738a5139afa81d0e9ca8a3d
Yin Deascentis <ydeascentis@gmail.com>
2017-04-24 16:58:12 -0400

    Edit new CSS; Add login.js

--- LoginTemplate/js/login.js
+++ LoginTemplate/js/login.js
@@ -1,5 +1,22 @@
 'user strict';
 $(document).ready(function() {
     //Login function
+    $('#login-form-link').click(function(e) {
+        e.preventDefault();
+        $("#login-form").delay(100).fadeIn(100);
+        $("#signup-form").fadeOut(100);
+        $('#signup-form-link').removeClass('active');
+        $(this).addClass('active');
+
+    });
+    //Signup function
+    $('#signup-form-link').click(function(e) {
+        e.preventDefault();
+        $("#signup-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+
+    });
 
 });
