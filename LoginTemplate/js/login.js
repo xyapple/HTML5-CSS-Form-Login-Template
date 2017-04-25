9f5a73f611f01c21ab3f40a0b2b79aca1a2eb8d9
Yin Deascentis <ydeascentis@gmail.com>
2017-04-24 22:15:45 -0400

    Try to add form validations, but it do not work as expect. Need to add inline error messages. For the next step, need to add validation.

--- LoginTemplate/js/login.js
+++ LoginTemplate/js/login.js
@@ -1,5 +1,7 @@
 'user strict';
 $(document).ready(function() {
+    //when page load 
+    $('#signup-form').hide();
     //Login function
     $('#login-submit').click(function() {
         var email = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
@@ -18,8 +20,9 @@ $(document).ready(function() {
     $("#login-form-link").click(function(e) {
 
         $("#login-form").fadeIn(100, function() {
-            $("#signup-form").fadeOut(100);
+            $("#signup-form").hide();
         })
+
         $('#signup-form-link').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
@@ -31,21 +34,62 @@ $(document).ready(function() {
         if ($("#first").val() == '' || $("#last").val() == '' || $("#registeremail").val() == '' || $("#registerpassword").val() == '' || $("#password_confirmation").val() == '') {
             alert("Please fill all fields...!!!!!!");
         } else if (!($("#registeremail").val()).match(email)) {
-            alert("Please enter valid Email...!!!!!!");
+            $('emailError').display("Please enter valid Email...!!!!!!");
         } else {
             alert("You have successfully Sign Up, Now you can login...!!!!!!");
         }
+
+
+        $('#register-submit').reset();
     });
     // On Click SignIn It Will Hide Registration Form and Display Login Form
     $("#signup-form-link").click(function(e) {
 
         $("#signup-form").fadeIn(100, function() {
 
-            $("#login-form").fadeOut(100);
+            $("#login-form").hide();
         });
 
         $('#signup-form-link').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
     });
+    // $("form[name='registration']").validate({
+    //     // Specify validation rules
+    //     rules: {
+    //         // The key name on the left side is the name attribute
+    //         // of an input field. Validation rules are defined
+    //         // on the right side
+    //         fist: "required",
+    //         last: "required",
+    //         username: "required",
+    //         registeremail: {
+    //             required: true,
+    //             // Specify that email should be validated
+    //             // by the built-in "email" rule
+    //             email: true
+    //         },
+    //         registerpassword: {
+    //             required: true,
+    //             minlength: 8
+    //         }
+    //     },
+    //     // Specify validation error messages
+    //     messages: {
+    //         firstname: "Please enter your firstname",
+    //         lastname: "Please enter your lastname",
+    //         username: "Please enter your username",
+    //         password: {
+    //             required: "Please provide a password",
+    //             minlength: "Your password must be at least 8 characters long"
+    //         },
+    //         email: "Please enter a valid email address"
+    //     },
+    //     // Make sure the form is submitted to the destination defined
+    //     // in the "action" attribute of the form when valid
+    //     submitHandler: function(form) {
+    //         form.submit();
+    //     }
+
+
 });
